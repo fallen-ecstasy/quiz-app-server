5f5e6abaa99ceca5b2f426b19a158c91811036ca
fallen-ecstasy <imfrost245@gmail.com>
2023-08-06 17:58:53 +0530

    init

---
+++ src/router/categoryRouter.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+const categoryRouter = Router();
+
+/* Category Routes */
+
+// ? - `POST /api/categories`
+// ? - `GET /api/categories/:id`
+// ? - `PUT /api/categories/:id`
+// ? - `DELETE /api/categories/:id`
+
+export default categoryRouter;
---
+++ src/router/questionsRouter.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+const questionRouter = Router();
+
+/* Question Routes */
+
+// ? `POST /api/quizzes/:quizId/questions`
+// ? `PUT /api/questions/:id`
+// ? `DELETE /api/questions/:id`
+
+export default questionRouter;
